d1f544020c2f2cd5280e9a9aeb9ddeb9fda9a5af
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-09-19 13:49:52 -0400

    Trimmed the JavaScript to follow DRY standards, and added comments to all files.

--- script.js
+++ script.js
@@ -13,44 +13,49 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Generate Random Password 
 function generatePassword(){
+  //Defines what characters will be used
   var char = "";
   var lowerChar = "abcdefghijklmnopqrstuvwxyz";
   var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numberChar = "0123456789";
   var specialChar = "!@#$%^&*()";
+  //Prompt to ask user for length of generated password
   var length = prompt("How long would you like your password? Password must be between 8-128 characters.");
-    length = parseInt(length);
     if (length < 8){
-      alert("Password must be longer than 7 characters.");
+      alert("Password length must be between 8 and 128.");
       return ""
     };
     if (length > 128){
-      alert("Password must be shorter than 129 characters.");
+      alert("Password length must be between 8 and 128.");
       return ""
     };
+    //Prompt to confirm use of lowercase characters in generated password
   var lower = confirm("Would you like to use lowercase characters?");
     if (lower) {
     char += lowerChar
     };
+    //Prompt to confirm use of uppercase characters in generated password
   var upper = confirm("Would you like to use uppercase characters?");
     if (upper) {
       char += upperChar
     };
+    //Prompt to confirm use of numbers in generated password
   var numbers = confirm("Would you like to use numbers?");
     if (numbers) {
       char += numberChar
     };
+    //Prompt to confirm use of special characters in generated password
   var specialChoice = confirm("Would you like to use special characters?");
     if (specialChoice) {
       char += specialChar
     };
+    //Loop to generate password
   var password = "";
   for (var i = 0; i < length; i++){
     password += char[Math.floor(Math.random() * char.length)];
   };
   return password;
-  
-  document.getElementById("password").value = password;
 };
 
