a77d51bdfdb0fde47df748a7127ff822795d2fd7
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-09-19 11:44:11 -0400

    Got bulk of code in JavaScript FINALLY working.

--- script.js
+++ script.js
@@ -14,8 +14,43 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
-  ///WRITE CODE HERE
-  var numCharacters = prompt("How many characters?");
-  return "hello:"
-}
+  var char = "";
+  var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+  var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numberChar = "0123456789";
+  var specialChar = "!@#$%^&*()";
+  var length = prompt("How long would you like your password? Password must be between 8-128 characters.");
+    length = parseInt(length);
+    if (length < 8){
+      alert("Password must be longer than 7 characters.");
+      return ""
+    };
+    if (length > 128){
+      alert("Password must be shorter than 129 characters.");
+      return ""
+    };
+  var lower = confirm("Would you like to use lowercase characters?");
+    if (lower) {
+    char += lowerChar
+    };
+  var upper = confirm("Would you like to use uppercase characters?");
+    if (upper) {
+      char += upperChar
+    };
+  var numbers = confirm("Would you like to use numbers?");
+    if (numbers) {
+      char += numberChar
+    };
+  var specialChoice = confirm("Would you like to use special characters?");
+    if (specialChoice) {
+      char += specialChar
+    };
+  var password = "";
+  for (var i = 0; i < length; i++){
+    password += char[Math.floor(Math.random() * char.length)];
+  };
+  return password;
+  
+  document.getElementById("password").value = password;
+};
 
